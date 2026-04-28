2f2e34b68ffbd6a62fa522464359ba21d3b860e8
yanamaruu <yana.s.79y@gmail.com>
2026-04-28 17:23:30 +0900

    log v6

--- script.js
+++ script.js
@@ -95,26 +95,6 @@ document.getElementById('todo-form').addEventListener('submit', async (e) => {
     inputEl.value = "";
 });
 
-// --- [2] 設定値（コンフィグ） ---
-const firebaseConfig = {
-  apiKey: "AIzaSyC07ffQp44CTQsGe6jzfo79FsI4ok5WrXc",
-  authDomain: "hazimeteno-todo-list.firebaseapp.com",
-  projectId: "hazimeteno-todo-list",
-  storageBucket: "hazimeteno-todo-list.firebasestorage.app",
-  messagingSenderId: "1028454995902",
-  appId: "1:1028454995902:web:6fdf7d1fb9c40a73ea0bef",
-  measurementId: "G-7EP1KTHWB8"
-};
-
-// --- [3] 初期化 ---
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// 2. HTML要素の取得
-const todoForm = document.getElementById('todo-form');
-const inputEl = document.getElementById('todo-input');
-const todoList = document.getElementById('todo-list');
-
 // 3. クラウド（Firestore）からデータをリアルタイムで読み出す
 // データベースが更新されるたびに、この中身が自動で実行されます
 const q = query(collection(db, "todos"), orderBy("createdAt", "desc"));
